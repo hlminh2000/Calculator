5bff6447e32a617b67667e0ae5ea04b73864f623
hlminh2000 <hlminh2000@gmail.com>
2016-03-19 17:38:45 -0400

    adds event listener to number buttons

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 
 
-$(document).ready(() => {
+window.addEventListener('load', () => {
+  Array.prototype.forEach.call(document.getElementsByClassName('number'), (button => {
+    button.addEventListener('click', () => {
+      console.log('hi');
+    })
+  }));
 
 });
