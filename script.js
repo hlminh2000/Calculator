59378f630d3808c79d23555fd7f3501893c61c0f
hlminh2000 <hlminh2000@gmail.com>
2016-03-20 11:06:45 -0400

    completes background logic

--- script.js
+++ script.js
@@ -2,10 +2,74 @@
 
 
 window.addEventListener('load', () => {
-  Array.prototype.forEach.call(document.getElementsByClassName('number'), (button => {
-    button.addEventListener('click', () => {
-      console.log('hi');
+
+  var prev_val = 0;
+  var new_val = 0;
+  var operating = false;
+  var lastOp = "";
+  var freshStart = true;
+
+  let calc_add = (new_val) => { return (prev_val) => {return new_val + prev_val}; }
+  let calc_sub = (new_val) => { return (prev_val) => {return new_val - prev_val}; }
+  let calc_mul = (new_val) => { return (prev_val) => {return new_val * prev_val}; }
+  let calc_div = (new_val) => { return (prev_val) => {return new_val / prev_val}; }
+  let evaluate = (prev_val, operation, new_val) => { return operation(prev_val)(new_val); }
+
+  let result_screen = document.getElementsByClassName('screen_result')[0];
+  let operation_screen = document.getElementsByClassName('screen_operation')[0];
+
+  Array.prototype.forEach.call(document.getElementsByClassName('number'), (digit => {
+    digit.addEventListener('click', () => {
+      if(freshStart === true){
+        prev_val = 0;
+        freshStart = false;
+      }
+      if(operating === false){
+        prev_val *= 10;
+        prev_val += Number(digit.innerHTML);
+        console.log(prev_val);
+        result_screen.innerHTML = prev_val;
+      }
+      else{
+        new_val *= 10;
+        new_val += Number(digit.innerHTML);
+        console.log(new_val);
+        result_screen.innerHTML = new_val;
+      }
     })
   }));
+
+  Array.prototype.forEach.call(document.getElementsByClassName('operator'), (operation => {
+    operation.addEventListener('click', () => {
+      operating = true;
+      if(lastOp != ""){
+        prev_val = evaluate(prev_val,
+          lastOp == 'add'       ? calc_add :
+          lastOp == 'minus'     ? calc_sub :
+          lastOp == 'multiply'  ? calc_mul : calc_div,
+          new_val)
+        new_val = 0;
+        console.log('result: ' + prev_val);
+      }
+      lastOp = operation.id;
+      freshStart = false;
+    })
+  }));
+
+  document.getElementById('equal').addEventListener('click', () => {
+    // console.log('hi');
+    if(operating === true){
+      prev_val = evaluate(prev_val,
+        lastOp == 'add'       ? calc_add :
+        lastOp == 'minus'     ? calc_sub :
+        lastOp == 'multiply'  ? calc_mul : calc_div,
+        new_val)
+        new_val = 0;
+        console.log('result: ' + prev_val);
+        lastOp = '';
+        operating = false;
+        freshStart = true;
+    }
+  })
 
 });
